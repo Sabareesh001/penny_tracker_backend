1af5a34f25a17abf8fffc0ac51b1ac271c7bff85
Sabareesh001 <hatrickda@gmail.com>
2025-06-04 15:57:37 +0530

    completed email verification for forgot password

--- internal/routes/v1/user_routes.go
+++ internal/routes/v1/user_routes.go
@@ -1,12 +1,13 @@
 package user_routes
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
+	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/authentication"
+	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/forgot"
+	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/insert"
 	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/registration"
 	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/verification"
-	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/insert"
-	"github.com/Sabareesh001/penny_tracker_backend/internal/routes/v1/user/authentication"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
@@ -16,6 +17,7 @@ func UserRoutes(router *gin.RouterGroup,DB *gorm.DB,redisClient *redis.Client){
 	verification.Verification(userRoutes,DB,redisClient)
 	insert.Insert(userRoutes,DB,redisClient)
     authentication.Auth(userRoutes,DB,redisClient)
+	forgot.Forgot(userRoutes,DB,redisClient)
 }
 
 
